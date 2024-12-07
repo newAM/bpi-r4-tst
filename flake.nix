c9a6fd2aefed58306f1eaffe501c2bb8f1d553db
Alex Martens <alex@thinglab.org>
2024-12-07 12:11:52 -0800

    flake: add checks

--- flake.nix
+++ flake.nix
@@ -17,12 +17,22 @@
             paramiko
             requests
 
-            # lint
-            flake8
-            flake8-bugbear
-            pep8-naming
+            # lint and formatting
+            ruff
           ]))
       ];
+    };
+
+    checks.x86_64-linux = {
+      ruff_format = pkgs.runCommand "ruff_format" {} ''
+        ${pkgs.ruff}/bin/ruff format --check ${self}
+        touch $out
+      '';
+
+      ruff_check = pkgs.runCommand "ruff_check" {} ''
+        ${pkgs.ruff}/bin/ruff check ${self}
+        touch $out
+      '';
     };
   };
 }
